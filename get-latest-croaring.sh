7f788356ab6de6837b518c18b7e40009e539abc6
Will Manning <will@willmanning.io>
2023-09-27 17:17:42 -0400

    script improvements

--- get-latest-croaring.sh
+++ get-latest-croaring.sh
@@ -3,22 +3,43 @@
 #  to build yourself.
 set -eou pipefail
 
-if [[ -z "${CROARING_REPO}" ]]; then
-    repo="/tmp/CRoaring"
-    git clone --depth 1 https://github.com/RoaringBitmap/CRoaring.git $repo
+script_path="$( cd "$(dirname "$0")" ; pwd -P )"
+arg_repo_path="${1-}"
+sibling_repo_path="${script_path}/../CRoaring"
+tmp_repo_path="/tmp/CRoaring"
+
+if [ ! -z "${arg_repo_path}" ]; then
+    if [ -d "${arg_repo_path}" ]; then
+        echo "Using CRoaring repo provided: ${arg_repo_path}"
+        repo=$arg_repo_path
+    else
+        echo "The provided CRoaring directory '${arg_repo_path})' does not exist"
+        exit 1
+    fi
+elif [ -d "${sibling_repo_path}" ]; then
+    echo "Using the sibling CRoaring directory at ${sibling_repo_path}"
+    repo=$sibling_repo_path
 else
-    repo="${CROARING_REPO}"
+    echo "Cloning CRoaring into ${tmp_repo_path}"
+    repo=$tmp_repo_path
+    git clone --depth 1 https://github.com/RoaringBitmap/CRoaring.git $repo
 fi
 
+# canonicalize the repo path
+repo="$( cd "${repo}" ; pwd -P )"
+
+# this is a hacky workaround for a bug in the amalgamation script
 mv $repo/.git $repo/.git.backup
-cd croaring
+# the amalgamation script writes to the current working directory
+cd $script_path/croaring
+# run the CRoaring amalgamation script
 $repo/amalgamation.sh
+# remove unnecessary demo and C++ amalgamation files
+rm amalgamation_demo.c amalgamation_demo.cpp roaring.hh
 
 # clean up
 cd -
 mv $repo/.git.backup $repo/.git
-rm croaring/amalgamation_demo.c croaring/amalgamation_demo.cpp croaring/roaring.hh
-
-if [[ -z "${CROARING_REPO}" ]]; then
-    rm -rf $repo
+if [ $repo == $tmp_repo_path ]; then
+    rm -rf $tmp_repo_path
 fi
